979314430bb8a655a311619fc2cdd5cde95e6ac4
JustBronwKid <larakid8080@gmail.com>
2025-08-06 06:41:28 +0630

    Fixx

--- src/modules/post copy/repositories/post.repository.ts
+++
@@ -1,9 +0,0 @@
-import { Post } from "../entities/post.entity";
-
-export interface PostRepository {
-    getAll(): Promise<Post[]>;
-    getById(id:number): Promise<Post | null>;
-    create(post : Omit<Post ,'id'>): Promise<Post>;
-    update(id: number, data: Partial<Post>): Promise<Post | null>;
-    delete(id: number): Promise<void>;
-}
